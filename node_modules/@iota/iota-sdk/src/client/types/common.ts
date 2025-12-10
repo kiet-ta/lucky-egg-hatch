fd07521864a46499be81caac763516f96cbdcc2a
kiet-ta <trananhkiet21082005@gmail.com>
2025-12-10 13:05:45 +0700

    refactor: remove file build node_modules and logic to sync frontend

--- node_modules/@iota/iota-sdk/src/client/types/common.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// Modifications Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-export type Order = 'ascending' | 'descending';
-export type Unsubscribe = () => Promise<boolean>;
--- web/node_modules/@iota/iota-sdk/src/client/apps-backend.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-export function getAppsBackend(): string {
-    return process.env.APPS_BACKEND as string;
-}
